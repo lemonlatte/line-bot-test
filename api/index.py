90157095e90ee7096bfc9743a849f6c3341f5887
Jim Yeh <jim_yeh@symphox.net>
2024-08-09 10:43:57 +0800

    add a first line bot webhook

--- api/index.py
+++ api/index.py
@@ -1,7 +1,80 @@
-from fastapi import FastAPI
+import os
+import sys
+from http.client import HTTPException
+from urllib.request import Request
+
+from fastapi import Depends, FastAPI
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    AsyncApiClient,
+    AsyncMessagingApi,
+    Configuration,
+    ReplyMessageRequest,
+    TextMessage,
+)
+from linebot.v3.webhook import WebhookParser
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
 
 app = FastAPI()
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+if channel_secret is None:
+    print("Specify LINE_CHANNEL_SECRET as environment variable.")
+    sys.exit(1)
+if channel_access_token is None:
+    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+    sys.exit(1)
+
+configuration = Configuration(access_token=channel_access_token)
+
+async_api_client = AsyncApiClient(configuration)
+line_bot_api = AsyncMessagingApi(async_api_client)
+parser = WebhookParser(channel_secret)
+
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+
+async def get_body_bytes(request: Request) -> bytes:
+    data: bytes = await request.body()
+    return data
+
+
+async def get_line_signature(request: Request) -> str:
+    signature: str = request.heeders.get("X-Line-Signature")
+    if not signature:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+    return signature
+
+
+@app.post("/api/line/webhook")
+async def webhook(
+    line_signature: str = Depends(get_line_signature),
+    request_data: bytes = Depends(get_body_bytes),
+) -> None:
+    # get request body as text
+    body = request_data.decode()
+
+    try:
+        events = parser.parse(body, line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    print("events", events)
+
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessageContent):
+            continue
+
+        await line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)],
+            )
+        )
